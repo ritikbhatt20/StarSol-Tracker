6f8c03dc33c2eb17dbdecdb4e60b555710347e62
mayank-dev07 <devmayank.hts@gmail.com>
2024-07-16 00:18:31 +0530

    reponsiveness updated

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -12,6 +12,7 @@ import {
   truncatedPublicKey,
 } from "@/utils/helper";
 import { useWalletStore } from "@/utils/zustand";
+import { sign } from "crypto";
 
 interface TokenBalance {
   mintAddress: string;
@@ -20,15 +21,14 @@ interface TokenBalance {
 
 const Transaction = () => {
   const publicKey = useWalletStore((state) => state.publicKey);
-  const setShow = useWalletStore((state)=>state.setShow)  
-  const show = useWalletStore((state)=>state.show)
+  const setShow = useWalletStore((state) => state.setShow);
+  const show = useWalletStore((state) => state.show);
   const [activeTab, setActiveTab] = useState("tokens");
   const [showTabs, setShowTabs] = useState(true);
   const [transactions, setTransactions] = useState<any[]>([]);
   const [tokenBalances, setTokenBalances] = useState<TokenBalance[]>([]);
   const [loadingTransactions, setLoadingTransactions] = useState(false);
   const [loadingTokenBalances, setLoadingTokenBalances] = useState(false);
-  const [transactionsFetched, setTransactionsFetched] = useState(false);
 
   useEffect(() => {
     console.log("Public key:", publicKey);
@@ -87,8 +87,8 @@ const Transaction = () => {
         5,
         3000
       );
-      if(signatures){
-        setShow(true)
+      if (signatures) {
+        setShow(true);
       }
 
       console.log(signatures);
@@ -116,7 +116,6 @@ const Transaction = () => {
         `transactions-${publicKey}`,
         JSON.stringify(filteredTransactions)
       );
-      setTransactionsFetched(true);
     } catch (error) {
       console.error("Error fetching transactions:", error);
     } finally {
@@ -175,143 +174,179 @@ const Transaction = () => {
   }
 
   return (
-    <div className={`min-h-screen flex-col py-24 container ${show? "flex":"hidden"}`}>
-      <div className="mb-4">
-        <ul
-          className="flex flex-wrap -mb-px text-sm font-medium text-center"
-          id="default-styled-tab"
-          role="tablist"
-        >
-          <li className="me-2" role="presentation">
-            <button
-              className={`inline-block p-4 border-b-2 rounded-t-lg ${
-                activeTab === "tokens"
-                  ? "text-yellow-500 border-yellow-500"
-                  : " hover:border-green-500 dark:hover:text-green-500  text-slate-600 border-slate-600"
-              }`}
-              onClick={() => handleTabClick("tokens")}
-              type="button"
-              role="tab"
-              aria-controls="tokens"
-              aria-selected={activeTab === "tokens"}
-            >
-              Tokens
-            </button>
-          </li>
-          <li className="me-2" role="presentation">
-            <button
-              className={`inline-block p-4 border-b-2 rounded-t-lg ${
-                activeTab === "transactions"
-                  ? "text-yellow-500 border-yellow-500"
-                  : " hover:border-green-500 dark:hover:text-green-500  text-slate-600 border-slate-600"
-              }`}
-              onClick={() => handleTabClick("transactions")}
-              type="button"
-              role="tab"
-              aria-controls="transactions"
-              aria-selected={activeTab === "transactions"}
-            >
-              Transactions
-            </button>
-          </li>
-        </ul>
+    <>
+      <div
+        className={`min-h-screen flex-col py-24 container ${
+          show && publicKey ? "flex" : "hidden"
+        }`}
+      >
+        <div className={"mb-4"}>
+          <ul
+            className="flex flex-wrap -mb-px text-sm font-medium text-center"
+            id="default-styled-tab"
+            role="tablist"
+          >
+            <li className="me-2" role="presentation">
+              <button
+                className={`inline-block p-4 border-b-2 rounded-t-lg ${
+                  activeTab === "tokens"
+                    ? "text-yellow-500 border-yellow-500"
+                    : " hover:border-green-500 dark:hover:text-green-500  text-slate-600 border-slate-600"
+                }`}
+                onClick={() => handleTabClick("tokens")}
+                type="button"
+                role="tab"
+                aria-controls="tokens"
+                aria-selected={activeTab === "tokens"}
+              >
+                Tokens
+              </button>
+            </li>
+            <li className="me-2" role="presentation">
+              <button
+                className={`inline-block p-4 border-b-2 rounded-t-lg ${
+                  activeTab === "transactions"
+                    ? "text-yellow-500 border-yellow-500"
+                    : " hover:border-green-500 dark:hover:text-green-500  text-slate-600 border-slate-600"
+                }`}
+                onClick={() => handleTabClick("transactions")}
+                type="button"
+                role="tab"
+                aria-controls="transactions"
+                aria-selected={activeTab === "transactions"}
+              >
+                Transactions
+              </button>
+            </li>
+          </ul>
+        </div>
+
+        {showTabs && show ? (
+          <div id="default-styled-tab-content" className="w-full mt-6">
+            {activeTab === "tokens" && (
+              <div className="" id="styled-tokens" role="tabpanel">
+                {loadingTokenBalances ? (
+                  <div className="text-center p-4">
+                    <span className="text-blue-500">
+                      Loading token balances...
+                    </span>
+                  </div>
+                ) : (
+                  <>
+                    {tokenBalances.length > 0 ? (
+                      <table className="w-full text-sm text-left rtl:text-righttext-gray-400">
+                        <thead className="text-xs text-gray-700  bg-slate-950 dark:text-gray-400">
+                          <tr>
+                            <th scope="col" className="px-6 py-3">
+                              Mint Address
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                              Total Balance
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {tokenBalances.map((token, index) => (
+                            <tr
+                              key={index}
+                              className="bg-slate-900 dark:border-gray-700"
+                            >
+                              <td className="px-6 py-4 font-medium text-green-600 whitespace-nowrap">
+                                {token.mintAddress}
+                              </td>
+                              <td className="px-6 py-4">{token.balance}</td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+                    ) : (
+                      <div className="text-center p-4">
+                        <span className="text-blue-500">
+                          No tokens available
+                        </span>
+                      </div>
+                    )}
+                  </>
+                )}
+              </div>
+            )}
+            {activeTab === "transactions" && (
+              <div className=" " id="styled-transactions" role="tabpanel">
+                {loadingTransactions ? (
+                  <div className="text-center p-4">
+                    <span className="text-blue-500">
+                      Loading transactions...
+                    </span>
+                  </div>
+                ) : (
+                  <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    {transactions.length > 0 ? (
+                      <table className="w-full text-sm text-left rtl:text-righttext-gray-400">
+                        <thead className="text-xs text-gray-700  bg-slate-950 dark:text-gray-400">
+                          <tr>
+                            <th scope="col" className="px-6 py-3">
+                              Transaction Signature
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                              Block
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                              Age
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                              Timestamp
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                              Result
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {transactions.map((tx, index) => (
+                            <tr
+                              key={index}
+                              className="bg-slate-900 dark:border-gray-700"
+                            >
+                              <td className="px-6 py-4 font-medium text-green-600 whitespace-nowrap">
+                                {tx.signature}
+                              </td>
+                              <td className="px-6 py-4">{tx.block}</td>
+                              <td className="px-6 py-4">{tx.age}</td>
+                              <td className="px-6 py-4">{tx.timestamp}</td>
+                              <td className="px-6 py-4">{tx.result}</td>
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+                    ) : (
+                      <div className="text-center p-4">
+                        <span className="text-blue-500">
+                          No transactions available
+                        </span>
+                      </div>
+                    )}
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
+        ) : (
+          <div className="text-center p-4">
+            <span className="text-blue-500">No tabs available</span>
+          </div>
+        )}
       </div>
 
-      {showTabs ? (
-        <div id="default-styled-tab-content" className="w-full mt-6">
-          {activeTab === "tokens" && (
-            <div className="" id="styled-tokens" role="tabpanel">
-              {loadingTokenBalances ? (
-                <div className="text-center p-4">
-                  <span className="text-blue-500">
-                    Loading token balances...
-                  </span>
-                </div>
-              ) : (
-                <table className="w-full text-sm text-left rtl:text-righttext-gray-400">
-                  <thead className="text-xs text-gray-700  bg-slate-950 dark:text-gray-400">
-                    <tr>
-                      <th scope="col" className="px-6 py-3">
-                        Mint Address
-                      </th>
-                      <th scope="col" className="px-6 py-3">
-                        Total Balance
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {tokenBalances.map((token, index) => (
-                      <tr
-                        key={index}
-                        className="bg-slate-900 dark:border-gray-700"
-                      >
-                        <td className="px-6 py-4 font-medium text-green-600 whitespace-nowrap">
-                          {token.mintAddress}
-                        </td>
-                        <td className="px-6 py-4">{token.balance}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              )}
-            </div>
-          )}
-          {activeTab === "transactions" && (
-            <div className=" " id="styled-transactions" role="tabpanel">
-              {loadingTransactions ? (
-                <div className="text-center p-4">
-                  <span className="text-blue-500">Loading transactions...</span>
-                </div>
-              ) : (
-                <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                  <table className="w-full text-sm text-left rtl:text-righttext-gray-400">
-                    <thead className="text-xs text-gray-700  bg-slate-950 dark:text-gray-400">
-                      <tr>
-                        <th scope="col" className="px-6 py-3">
-                          Transaction Signature
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                          Block
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                          Age
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                          Timestamp
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                          Result
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {transactions.map((tx, index) => (
-                        <tr
-                          key={index}
-                          className="bg-slate-900 dark:border-gray-700"
-                        >
-                          <td className="px-6 py-4 font-medium text-green-600 whitespace-nowrap">
-                            {tx.signature}
-                          </td>
-                          <td className="px-6 py-4">{tx.block}</td>
-                          <td className="px-6 py-4">{tx.age}</td>
-                          <td className="px-6 py-4">{tx.timestamp}</td>
-                          <td className="px-6 py-4">{tx.result}</td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
-              )}
-            </div>
-          )}
-        </div>
-      ) : (
-        <div className="text-center p-4">
-          <span className="text-blue-500">No tabs available</span>
-        </div>
-      )}
-    </div>
+      <div
+        className={`text-center p-4 ${
+          show && publicKey ? "hidden" : "flex"
+        } min-h-[60vh] justify-center items-center`}
+      >
+        <span className="text-blue-500">
+          No transactions to show connect to wallet
+        </span>
+      </div>
+    </>
   );
 };
 
